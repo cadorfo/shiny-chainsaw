4a31444c6770b7538f4cbeea1bf90f6b9d5d2e74
Carlos Nascimento <carlos.nascimento@adtsys.com.br>
2017-03-27 17:42:29 -0300

    Adiciona editar

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,10 +1,46 @@
 defmodule Web.TopicController do
   use Web.Web, :controller
 
+  alias Web.Topic
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render(conn, "index.html", topics: topics)
+  end
+
   def new(conn, params) do
-    IO.inspect conn
-    IO.puts "  --- "
-    IO.inspect conn
-    render conn, "new.html"
+    changeset = Topic.changeset(%Topic{},%{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn,%{"topic" => topic}) do
+     %{"title" => title} = topic
+     changeset = Topic.changeset(%Topic{}, topic)
+     case Repo.insert(changeset) do
+       {:ok, post} ->
+          conn
+          |> put_flash(:info, "Topic created")
+          |> redirect(to: topic_path(conn,:index))
+       {:error, changeset} -> render conn, "new.html", changeset: changeset
+     end
+  end
+
+  def edit(conn, %{"id"=> topic_id}) do
+    topic = Repo.get(Topic,topic_id)
+    changeset = Topic.changeset(topic)
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn,%{"topic" => topic, "id" => id}) do
+     %{"title" => title} = topic
+     topic_record = Repo.get(Topic,id)
+     changeset = Topic.changeset(topic_record, topic)
+     case Repo.update(changeset) do
+       {:ok, post} ->
+          conn
+          |> put_flash(:info, "Topic edited")
+          |> redirect(to: topic_path(conn,:index))
+       {:error, changeset} -> render conn, "edit.html", changeset: changeset, topic: topic_record
+     end
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -16,8 +16,12 @@ defmodule Web.Router do
   scope "/", Web do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+
+    get "/", TopicController, :index
     get "/topics/new", TopicController, :new
+    post "/topics", TopicController, :create
+    get "/topics/:id/edit", TopicController, :edit
+    put "/topics/:id", TopicController, :update
   end
 
   # Other scopes may use custom stacks.
