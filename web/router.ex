99c926ff3e6617b9f8b2af1d1b93606bdd1627ae
Carlos Nascimento <cadorfo@gmail.com>
2017-04-02 11:03:50 -0300

    Create signout

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Web.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Web.Plugs.SetUser
   end
 
   pipeline :api do
@@ -24,8 +25,9 @@ defmodule Web.Router do
   scope "/auth", Web do
     pipe_through :browser
 
+    get "/signout", AuthController, :signout
     get "/:provider", AuthController, :request
-    get "/:provider/callback", AuthController, :callback
+    get "/:provider/callback", AuthController, :callback    
   end
 
   # Other scopes may use custom stacks.
